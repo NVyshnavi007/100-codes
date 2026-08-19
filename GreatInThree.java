660089afcdcbfad0c47920a01889650a9f1d8d7f
NVyshnavi007 <nvyshnavinannapaneni@gmail.com>
2026-08-19 19:08:05 +0530

    Added new programs

---
+++ GreatInThree.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+class GreatInThree{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2=sc.nextInt();
+        System.out.println("Enter the third number: ");
+        int num3=sc.nextInt();
+        if(num1>num2 && num1>num3)
+            {
+            System.out.println(num1);
+            }
+        else if(num2>num1 && num2>num3)
+            {
+            System.out.println(num2);
+            }
+        else
+            {
+            System.out.println(num3);
+            }
+        sc.close();
+    }
+}
